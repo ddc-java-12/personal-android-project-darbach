01c30562d0f9b7ced93f9457e4f3b139a8b29730
darbach <danielarbach@gmail.com>
2021-03-18 16:09:29 -0600

    Added initial UI controller + VM to demonstrate DB.

--- app/src/main/java/edu/cnm/deepdive/dicecrunch/DiceCrunchApplication.java
+++ app/src/main/java/edu/cnm/deepdive/dicecrunch/DiceCrunchApplication.java
@@ -2,18 +2,26 @@ package edu.cnm.deepdive.dicecrunch;
 
 import android.app.Application;
 import com.facebook.stetho.Stetho;
+import edu.cnm.deepdive.dicecrunch.service.DiceCrunchDatabase;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Initializes (in the {@link #onCreate()} method) application-level resources. This class
  * <strong>must</strong> be referenced in {@code AndroidManifest.xml}, or it will not be loaded and
  * used by the Android system.
  */
-public class AppStarterApplication extends Application {
+public class DiceCrunchApplication extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
     Stetho.initializeWithDefaults(this);
+    DiceCrunchDatabase.setContext(this);
+    DiceCrunchDatabase.getInstance()
+        .getDieDao()
+        .delete()
+        .subscribeOn(Schedulers.io())
+        .subscribe();
   }
 
 }
--- app/src/main/java/edu/cnm/deepdive/dicecrunch/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicecrunch/MainActivity.java
@@ -1,13 +1,22 @@
 package edu.cnm.deepdive.dicecrunch;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.dicecrunch.databinding.ActivityMainBinding;
+import edu.cnm.deepdive.dicecrunch.viewmodel.CalculatorViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+  private CalculatorViewModel viewModel;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+    viewModel = new ViewModelProvider(this).get(CalculatorViewModel.class);
+    binding.setViewModel(viewModel);
+    binding.setLifecycleOwner(this);
   }
 }

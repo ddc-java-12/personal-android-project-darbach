143a65aaaffb17ffa6bd000eced4ce832816b926
darbach <danielarbach@gmail.com>
2021-03-24 14:43:25 -0600

    Added Bottom Navigation.

--- app/src/main/java/edu/cnm/deepdive/dicecrunch/DiceCrunchApplication.java
+++ app/src/main/java/edu/cnm/deepdive/dicecrunch/DiceCrunchApplication.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive.dicecrunch;
 import android.app.Application;
 import com.facebook.stetho.Stetho;
 import edu.cnm.deepdive.dicecrunch.service.DiceCrunchDatabase;
+import edu.cnm.deepdive.dicecrunch.service.GoogleSignInService;
 import io.reactivex.schedulers.Schedulers;
 
 /**
@@ -15,6 +16,7 @@ public class DiceCrunchApplication extends Application {
   @Override
   public void onCreate() {
     super.onCreate();
+    GoogleSignInService.setContext(this);
     Stetho.initializeWithDefaults(this);
     DiceCrunchDatabase.setContext(this);
     DiceCrunchDatabase.getInstance()
--- app/src/main/java/edu/cnm/deepdive/dicecrunch/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicecrunch/controller/MainActivity.java
@@ -1,13 +1,16 @@
 package edu.cnm.deepdive.dicecrunch.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import edu.cnm.deepdive.dicecrunch.R;
-import edu.cnm.deepdive.dicecrunch.databinding.ActivityMainBinding;
+import edu.cnm.deepdive.dicecrunch.service.GoogleSignInService;
 import edu.cnm.deepdive.dicecrunch.viewmodel.CalculatorViewModel;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,9 +22,30 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-    viewModel = new ViewModelProvider(this).get(CalculatorViewModel.class);
-    binding.setViewModel(viewModel);
-    binding.setLifecycleOwner(this);
+    setContentView(R.layout.activity_main);
+    BottomNavigationView navView = findViewById(R.id.nav_view);
+    // Passing each menu ID as a set of Ids because each
+    // menu should be considered as top level destinations.
+    AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        R.id.navigation_calculator, R.id.navigation_history, R.id.navigation_dice_tray)
+        .build();
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    NavigationUI.setupWithNavController(navView, navController);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.options, menu);
+    return true;
+  }
+
+  private void signOut() {
+    GoogleSignInService
+        .getInstance()
+        .signOut()
+        .addOnCompleteListener((ignored) ->
+            startActivity(new Intent(this, LoginActivity.class)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK)));
   }
 }
